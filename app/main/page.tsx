136314bbcebc0e9377e2056cdf79af190ee063af
Prason KC <prasonkc07@gmail.com>
2025-11-16 09:47:31 +0545

    Forum Card

---
+++ app/main/page.tsx
@@ -0,0 +1,15 @@
+import ForumCard from "@/components/ForumCard";
+
+const page = () => {
+  return (
+    <div>
+        <div className="banner">
+            <div className="slider">
+                <div className="item"></div>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default page;
---
+++ components/ForumCard.tsx
@@ -0,0 +1,32 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+const ForumCard = () => {
+  return (
+    <div>
+      <Card className="w-[15%] cursor-pointer border-2 shadow-xl shadow-gray-100">
+        <CardHeader className=" flex items-center ">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+
+          <CardTitle className="text-lg">Lorem ipsum dolor </CardTitle>
+        </CardHeader>
+
+        <CardContent>
+          <p className="text-sm leading-relaxed max-w-prose space-y-2 space-x-1.5">
+            Card Content is a content that im going to write within tie card
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore
+            corrupti rem asperiores praesentium expedita totam officia quaerat,
+            porro dolorum natus suscipit iste?Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Ut, architecto. Lorem ipsum dolor sit
+            amet.
+          </p>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default ForumCard;
