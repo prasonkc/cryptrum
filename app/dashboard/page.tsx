55df2396fc03010b07114fab2941488c3ad0bb96
Prason KC <prasonkc07@gmail.com>
2025-11-25 15:51:48 +0545

    Dashboard card

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+"use client"
+import React from "react";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
 
-const page = () => {
+import { useTheme } from "next-themes";
+
+const Dashboard = () => {
+  const { resolvedTheme } = useTheme();
   return (
-    <div>
-      
+  <div >
+      <Card className={`outline-none border-none gap-4 w-200 m-auto ${resolvedTheme == "dark"? "shadow-white": "shadow-black"}`}>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardFooter className="flex gap-2">
+          <Badge variant="outline">Outline</Badge>
+          <Badge variant="outline">Hello World</Badge>
+        </CardFooter>
+      </Card>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Dashboard;
