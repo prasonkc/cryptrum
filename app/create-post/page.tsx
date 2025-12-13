993afd33f5c77d5c49f4b8bf334afc5a2427e5db
Prason KC <prasonkc07@gmail.com>
2025-12-13 10:58:07 +0545

    Setup rich text editor

--- app/create-post/page.tsx
+++ app/create-post/page.tsx
@@ -2,11 +2,46 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { authClient } from "@/lib/auth-client";
+import { SerializedEditorState } from "lexical";
+import { Editor } from "@/components/blocks/editor-00/editor";
+
+// Declare initial values for text editor
+const initialValue = {
+  root: {
+    children: [
+      {
+        children: [
+          {
+            detail: 0,
+            format: 0,
+            mode: "normal",
+            style: "",
+            text: "Hello World 🚀",
+            type: "text",
+            version: 1,
+          },
+        ],
+        direction: "ltr",
+        format: "",
+        indent: 0,
+        type: "paragraph",
+        version: 1,
+      },
+    ],
+    direction: "ltr",
+    format: "",
+    indent: 0,
+    type: "root",
+    version: 1,
+  },
+} as unknown as SerializedEditorState;
 
 const CreatePost = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const { data: session } = authClient.useSession();
+  const [editorState, setEditorState] =
+    useState<SerializedEditorState>(initialValue);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -20,13 +55,13 @@ const CreatePost = () => {
       });
 
       let postId = await res.data.post.id;
-      postId = Number(postId)
+      postId = Number(postId);
 
       window.location.href = `/post/${postId}`;
     } catch (err) {
       console.log(err);
     }
-  }
+  };
 
   return (
     <div className="max-w-xl mx-auto p-6">
@@ -39,18 +74,16 @@ const CreatePost = () => {
           onChange={(e) => setTitle(e.target.value)}
           className="w-full border rounded p-2"
         />
-        <textarea
-          placeholder="Content"
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-          className="w-full border rounded p-2 h-40"
+        <Editor
+          editorSerializedState={editorState}
+          onSerializedChange={(value) => setEditorState(value)}
         />
         <button type="submit" className="px-4 py-2 bg-black text-white rounded">
           Create
         </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default CreatePost
+export default CreatePost;
