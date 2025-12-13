6c379111c1a29746c9b78868e603b986149a95d3
Prason KC <prasonkc07@gmail.com>
2025-12-13 11:43:47 +0545

    UPDATE TO NEXT 16.0.10 (IMPORTANT)

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -7,12 +7,40 @@ import { AppDispatch, RootState } from "@/redux/store";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { fetchPosts } from "@/redux/fetchPosts";
+import { $generateHtmlFromNodes } from "@lexical/html";
+import { HeadingNode, QuoteNode } from "@lexical/rich-text";
+import { ListNode, ListItemNode } from "@lexical/list";
+import { LinkNode } from "@lexical/link";
+import { createHeadlessEditor } from "@lexical/headless";
 
 const Post = () => {
   const pathname = usePathname();
   const id = pathname.split("/").pop();
   const dispatch = useDispatch<AppDispatch>();
   const posts = useSelector((state: RootState) => state.posts.value);
+
+const lexicalToHtml = (serializedState: string) => {
+  const editor = createHeadlessEditor({
+    nodes: [
+      HeadingNode,
+      QuoteNode,
+      ListNode,
+      ListItemNode,
+      LinkNode,
+    ],
+  });
+
+  const editorState = editor.parseEditorState(serializedState);
+  editor.setEditorState(editorState);
+
+  let html = "";
+  editorState.read(() => {
+    html = $generateHtmlFromNodes(editor);
+  });
+
+  return html;
+};
+
 
   useEffect(() => {
     if (posts.length === 0) {
