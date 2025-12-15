ed8b6adb7290a086a035f73111b8b9aba9df4437
Prason KC <prasonkc07@gmail.com>
2025-12-15 20:31:39 +0545

    More plugins

--- components/blocks/editor-00/plugins.tsx
+++ components/blocks/editor-00/plugins.tsx
@@ -1,16 +1,38 @@
+"use client"
+
 import { useState } from "react"
 import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary"
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin"
+
+// Core Lexical Plugins
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin"
+import { CheckListPlugin } from "@lexical/react/LexicalCheckListPlugin"
+import { TabIndentationPlugin } from "@lexical/react/LexicalTabIndentationPlugin"
+
+// Shadcn-Editor UI Components/Plugins
 import { ToolbarPlugin } from "@/components/editor/plugins/toolbar/toolbar-plugin"
 import { BlockFormatDropDown } from "@/components/editor/plugins/toolbar/block-format-toolbar-plugin"
 import { FormatHeading } from "@/components/editor/plugins/toolbar/block-format/format-heading"
-import { ContentEditable } from "@/components/editor/editor-ui/content-editable"
 import { FormatParagraph } from "@/components/editor/plugins/toolbar/block-format/format-paragraph"
+import { ContentEditable } from "@/components/editor/editor-ui/content-editable"
 
-export function Plugins()  {
+import { ElementFormatToolbarPlugin } from "@/components/editor/plugins/toolbar/element-format-toolbar-plugin"
+import { FontColorToolbarPlugin } from "@/components/editor/plugins/toolbar/font-color-toolbar-plugin"
+import { FontBackgroundToolbarPlugin } from "@/components/editor/plugins/toolbar/font-background-toolbar-plugin"
+
+
+export function Plugins() {
+  const [floatingAnchorElem, setFloatingAnchorElem] = useState<HTMLDivElement | null>(null)
+
+  const onRef = (_floatingAnchorElem: HTMLDivElement) => {
+    if (_floatingAnchorElem !== null) {
+      setFloatingAnchorElem(_floatingAnchorElem)
+    }
+  }
+
   return (
     <div className="relative">
-      {/* toolbar plugins */}
+      {/* 1. Toolbar Section */}
       <ToolbarPlugin>
         {() => (
           <div className="sticky top-0 z-10 flex gap-2 overflow-auto border-b bg-background p-1">
@@ -18,24 +40,36 @@ export function Plugins()  {
               <FormatParagraph />
               <FormatHeading levels={["h1", "h2", "h3"]} />
             </BlockFormatDropDown>
+            
+            {/* Standard Text Formatting (Bold, Italic, etc.) */}
+            {/* <FontColorToolbarPlugin />
+            <FontBackgroundToolbarPlugin /> */}
+            
+            {/* Alignment and Lists */}
+            <ElementFormatToolbarPlugin />
           </div>
         )}
       </ToolbarPlugin>
 
-      <div className="relative">
+      {/* 2. Editor Surface */}
+      <div className="relative" ref={onRef}>
         <RichTextPlugin
           contentEditable={
-            <div className="">
-              <div className="">
+            <div className="editor-scroller">
+              <div className="editor">
                 <ContentEditable placeholder={"Start typing ..."} />
               </div>
             </div>
           }
           ErrorBoundary={LexicalErrorBoundary}
         />
-        {/* editor plugins */}
+
+        {/* 3. Logic Plugins*/}
+        <HistoryPlugin />
+        <CheckListPlugin />
+        <TabIndentationPlugin />
+        
       </div>
-      {/* actions plugins */}
     </div>
   )
 }
