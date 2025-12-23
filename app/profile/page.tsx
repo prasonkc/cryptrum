3c8e6c25ed8742eb6862854e9f7cf5214f9f2491
Prason KC <prasonkc07@gmail.com>
2025-12-23 22:36:42 +0545

    Access initials for fallback

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,15 +1,25 @@
+"use client"
 import React from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Edit2 } from 'lucide-react'
+import { authClient } from '@/lib/auth-client'
 
 const Profile = () => {
+  const { data: session } = authClient.useSession();
+
+  if(!session){
+    return
+  }
+
+  const name = session?.user.name;
+  const initial = name.charAt(0).toUpperCase() + name.charAt(1).toUpperCase();
   return (
     <div className='w-full h-screen'>
       <div className='max-w-[70%] p-10 my-10 rounded-2xl h-[30%] bg-gray-950 m-auto'>
-          <Avatar className='w-20 h-20 bg-black cursor-pointer'>
+          <Avatar className='w-20 h-20 cursor-pointer'>
             <AvatarImage src={""}/>
 
-            <AvatarFallback></AvatarFallback>
+            <AvatarFallback>{initial}</AvatarFallback>
           </Avatar>
       </div>
     </div>
