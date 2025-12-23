1029c7dbf21dcf91abe3f4053d15bee6e4db06b8
Prason KC <prasonkc07@gmail.com>
2025-12-23 22:31:58 +0545

    Working on profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Edit2 } from 'lucide-react'
 
 const Profile = () => {
   return (
-    <div>
-      Your Profile here
+    <div className='w-full h-screen'>
+      <div className='max-w-[70%] p-10 my-10 rounded-2xl h-[30%] bg-gray-950 m-auto'>
+          <Avatar className='w-20 h-20 bg-black cursor-pointer'>
+            <AvatarImage src={""}/>
+
+            <AvatarFallback></AvatarFallback>
+          </Avatar>
+      </div>
     </div>
   )
 }
